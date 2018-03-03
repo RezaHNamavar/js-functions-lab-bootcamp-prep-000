6cd05b20ceb2028d14ed320fc1d5edf73106050a
rezahnamavar@gmail.com Handley-Namavar <rezahnamavar@gmail.com>
2018-03-03 00:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // write your code below!
-function happyHolidays()
-  
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  `It's ${days} days until ${holiday}!`
+}
